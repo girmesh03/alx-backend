00f80fb1d1bc46995e24c57972a8136deea97d5e
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-22 20:50:29 +0300

    100-lfu_cache.py updated #1

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -10,44 +10,29 @@ class LFUCache(BaseCaching):
     from BaseCaching and is a caching system """
 
     def __init__(self):
-        """ Initialize LFU Caching """
+        """ Initialize LFUCache """
         super().__init__()
-        self.lfu = []
-        self.lfu_count = {}
+        self.lfu = {}
+        self.lfu_counter = 0
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key and item:
             if key in self.cache_data:
                 self.cache_data[key] = item
-                self.lfu_count[key] += 1
-                self.lfu.remove(key)
+                self.lfu[key] += 1
             else:
                 if len(self.cache_data) >= self.MAX_ITEMS:
-                    min_value = min(self.lfu_count.values())
-                    min_keys = [k for k in self.lfu_count
-                                if self.lfu_count[k] == min_value]
-                    if len(min_keys) > 1:
-                        for k in self.lfu:
-                            if k in min_keys:
-                                self.cache_data.pop(k)
-                                self.lfu_count.pop(k)
-                                self.lfu.remove(k)
-                                break
-                    else:
-                        self.cache_data.pop(min_keys[0])
-                        self.lfu_count.pop(min_keys[0])
-                        self.lfu.remove(min_keys[0])
-                        print("DISCARD: {}".format(min_keys[0]))
+                    min_key = min(self.lfu, key=self.lfu.get)
+                    self.cache_data.pop(min_key)
+                    self.lfu.pop(min_key)
+                    print("DISCARD: {}".format(min_key))
                 self.cache_data[key] = item
-                self.lfu_count[key] = 0
-            self.lfu.append(key)
+                self.lfu[key] = 1
 
     def get(self, key):
         """ Get an item by key """
         if key is None or key not in self.cache_data.keys():
             return None
-        self.lfu_count[key] += 1
-        self.lfu.remove(key)
-        self.lfu.append(key)
+        self.lfu[key] += 1
         return self.cache_data[key]
